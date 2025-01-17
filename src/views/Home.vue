343f01447fb47adfa7d97fc7f97ccc9bf2142d57
neve99 <165163178+neve99@users.noreply.github.com>
2025-01-17 11:30:37 +0100

    fail to merge

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,5 @@
 <template>
   <div class="home">
-    <h1>All destinations</h1>
-    <router-link
-      v-for="destination in destinations"
-      :key="destination.id"
-      :to="destination.slug"
-    >
-      <h2>{{ destination.name }}</h2>
-      <img :src="`/images/${destination.image}`" alt="">
-    </router-link>
+    <h1>Home</h1>
   </div>
 </template>
-
-<script>
-import sourceData from '@/data.json'
-export default {
-
-  data() {
-    return {
-      destinations: sourceData.destinations
-    }
-  }
-}
-</script>
