a811f585555eac907ea7ac114a30d650d90d8ba3
neve99 <165163178+neve99@users.noreply.github.com>
2025-01-17 11:17:53 +0100

    single out navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
-<script setup>
-
+<script>
+  import TheNavigation from '@/components/TheNavigation.vue'
+  export default{
+    components: {
+      TheNavigation
+    }
+  }
 </script>
 
 <template>
+  <TheNavigation />
   <div>
-    <div id="nav">
-      <router-link to="/">Home</router-link> 
-      <router-link to="/brazil">Brazil</router-link>
-      <router-link to="/hawaii">Hawaii</router-link>
-      <router-link to="/jamaica">Jamaica</router-link>
-      <router-link to="/panama">Panama</router-link>
-    </div>
+    
     <div class="container">
       <router-view />
     </div>
@@ -18,17 +18,4 @@
 
 </template>
 
-<!-- <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style> -->
+
